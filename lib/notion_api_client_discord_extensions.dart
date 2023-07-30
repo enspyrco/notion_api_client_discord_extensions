abeaa358086615092c933ea2cfa005776f8d4dfd
Nick Meinhold <nick@enspyr.co>
2023-07-31 00:33:04 +1000

    first commit

---
+++ lib/notion_api_client_discord_extensions.dart
@@ -0,0 +1,11 @@
+/// Extensions on models from notion_api_client, providing String output
+/// appropriate for Discord.
+///
+///
+library;
+
+import 'package:notion_api_client/notion_api_client.dart';
+
+extension PageExtension on Page {
+  toDiscordMessage() {}
+}
