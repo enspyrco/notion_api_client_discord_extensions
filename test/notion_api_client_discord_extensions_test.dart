af3aec9be24a7955c1c602d7263cca5bb9e90012
Nick Meinhold <nick@enspyr.co>
2023-08-02 00:41:45 +1000

    Update README, add extension to Page & test

--- test/notion_api_client_discord_extensions_test.dart
+++ test/notion_api_client_discord_extensions_test.dart
@@ -1,13 +1,21 @@
+import 'dart:convert';
+
+import 'package:notion_api_client/notion_api_client.dart';
+import 'package:notion_api_client_discord_extensions/notion_api_client_discord_extensions.dart';
 import 'package:test/test.dart';
 
-void main() {
-  group('A group of tests', () {
-    setUp(() {
-      // Additional setup goes here.
-    });
+import 'data/page_example.dart';
 
-    test('First Test', () {
-      expect(true, isTrue);
-    });
+void main() {
+  test('example page prints appropriate Discord message', () async {
+    final json = jsonDecode(examplePageJsonString);
+    final page = Page.fromJson(json);
+
+    String message = page.toDiscordMessage();
+    List<String> messageLines = message.split('\n');
+
+    expect(messageLines.elementAt(0),
+        'created by: Instance of \'PartialUserObject\'');
+    expect(messageLines.elementAt(1), 'on: 2022-10-24 22:54:00.000Z');
   });
 }
